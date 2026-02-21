71e8c706b7f3c7b0bd8f8fa328a57a5f50214f37
Billy Lachenal <billylachenal@gmail.com>
2026-02-20 23:36:33 -0600

    initial functions created

--- index.js
+++ index.js
@@ -10,3 +10,21 @@ const NAMES = ["Alice", "Bob", "Carol", "Dave", "Eve"];
 const OCCUPATIONS = ["Writer", "Teacher", "Programmer", "Designer", "Engineer"];
 const PRICE_RANGE = { min: 20, max: 200 };
 const NUM_FREELANCERS = 100;
+ 
+function makeFreelancer() {
+  // TODO
+  const name = NAMES[Math.floor(Math.random() * NAMES.length)];
+  const occupation =  OCCUPATIONS[Math.floor(Math.random() * OCCUPATIONS.length)];
+  const price = Math.floor(Math.random() * (PRICE_RANGE.max - PRICE_RANGE.min) + PRICE_RANGE.min);
+  return {name, occupation, price};
+}
+const freelanceContractor = [];
+for(let i = 0; i < NUM_FREELANCERS; i++){
+    freelanceContractor[i] = makeFreelancer();
+}
+function averageCost(freelanceContractor){
+    let sum = freelanceContractor.reduce((acc, el) => {
+        return acc + el.price;
+    }, 0);
+    return sum / freelanceContractor.length; 
+}
